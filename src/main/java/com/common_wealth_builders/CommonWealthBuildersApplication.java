7826dc7db68eeab3e5246f55d958a23a162410a9
David Baba <davidbabasalifu2020@gmail.com>
2026-02-03 12:05:39 +0100

    Initial commit

---
+++ src/main/java/com/common_wealth_builders/CommonWealthBuildersApplication.java
@@ -0,0 +1,13 @@
+package com.common_wealth_builders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonWealthBuildersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonWealthBuildersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common_wealth_builders/CommonWealthBuildersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common_wealth_builders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWealthBuildersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
