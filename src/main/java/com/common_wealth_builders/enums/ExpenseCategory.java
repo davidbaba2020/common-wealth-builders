50f66537a753994539a7c1c3500b63a5c5edd115
David <david.baba@simplifysynergy.com>
2026-02-05 16:13:44 +0100

    Updated app

---
+++ src/main/java/com/common_wealth_builders/enums/ExpenseCategory.java
@@ -0,0 +1,12 @@
+package com.common_wealth_builders.enums;
+
+public enum ExpenseCategory {
+    OPERATIONAL,
+    ADMINISTRATIVE,
+    MAINTENANCE,
+    UTILITIES,
+    SALARY,
+    MISCELLANEOUS,
+    EVENT,
+    WELFARE
+}
---
+++ src/main/java/com/common_wealth_builders/enums/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.common_wealth_builders.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    VERIFIED,
+    REJECTED,
+    CANCELLED
+}
